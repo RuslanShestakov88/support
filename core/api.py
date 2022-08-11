162c26cceb8e093d6bb9aa36a888347aa184e36d
RuslanShestakov88 <rshestakov88@gmail.com>
2022-08-12 01:56:29 +0300

    ticket api with serializer

--- core/api.py
+++ core/api.py
@@ -1,13 +1,62 @@
-from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from core.models import Ticket
+from rest_framework import serializers
+from rest_framework.decorators import api_view
+from authentication.models import Role
 
 
-class TicketServivce:
-    def get_all_tickets(self) -> dict:
-        return {}
+User = get_user_model()
 
 
+def user_as_dict(user: User) -> dict:
+    return{
+        "username": user.username,
+        "email": user.email,
+        "phone": user.phone,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "age": user.age,
+    }
+
+
+def ticket_as_dict(ticket: Ticket) -> dict:
+    return {
+        "id": ticket.id,
+        "theme": ticket.theme,
+        "discription": ticket.description,
+        
+        "resolved": ticket.resolved,
+        "created_at": ticket.created_at,
+        "updated_at": ticket.updated_at,
+    }
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    role = RoleSerializer()
+    
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    operator = UserSerializer()
+    client = UserSerializer()
+    
+    class Meta:
+        model = Ticket
+        fields = "__all__"
+
+
+@api_view(["GET"])
 def get_all_tickets(requst):
-    ticket_service = TicketServivce()
-    data = ticket_service.get_all_tickets()
-
-    return JsonResponse(data)
+    tickets = Ticket.objects.all()
+    data = TicketSerializer(tickets, many=True).data
+    return Response(data=data)
