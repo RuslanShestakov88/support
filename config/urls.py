62306226fb298d54788ec874ace0da68ccfdd6db
RuslanShestakov88 <rshestakov88@gmail.com>
2022-08-15 14:00:40 +0300

    code_revue

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tickets/", include("core.urls")),
-    path("exchange_rates/", include("exchange_rates.urls"))
+    path("exchange_rates/", include("exchange_rates.urls")),
 ]
--- exchange_rates/urls.py
+++ exchange_rates/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from exchange_rates.api import btc_usd, history
 
-
 urlpatterns = [
     path("btc_usd/", btc_usd),
     path("history/", history),
