da4f64c6c76522ae7d4292a4979d41d183d87671
RuslanShestakov88 <rshestakov88@gmail.com>
2022-08-23 22:43:57 +0300

    cange repr user

--- authentication/admin.py
+++ authentication/admin.py
@@ -8,7 +8,7 @@ class UserAdmin(admin.ModelAdmin):
     exclude = ["user_permissions", "groups"]
     readonly_fields = ["password", "last_login"]
     list_filter = ["age"]
-    list_display = ["age"]
+    list_display = ["username"]
 
 
 @admin.register(Role)
