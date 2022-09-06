6693df0d62b4256efc1d8976cbcfa4ef50d2ea3f
RuslanShestakov88 <rshestakov88@gmail.com>
2022-09-07 02:08:28 +0300

    cade ravue

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
-    path("auth/", include('djoser.urls.jwt')),
+    path("auth/", include("djoser.urls.jwt")),
     path("tickets/", include("core.urls")),
     path("exchange_rates/", include("exchange_rates.urls")),
 ]
